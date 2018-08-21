3790d4d7c7397617b72591fc768fc6dfbc109747
Walter Jardim <walterjardim@gmail.com>
2018-08-21 15:51:30 -0300

    initial commit with config server server, discovery server and api gateway working

---
+++ discovery-server/src/main/java/com/mv/challenge/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package com.mv.challenge.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+}
